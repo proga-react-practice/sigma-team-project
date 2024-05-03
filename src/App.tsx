e7421c3feb59d328a54c553a62016254530f09b3
OstapivIvan <ostapivivan05@gmail.com>
2024-05-03 12:56:33 +0300

    Move paths and router to separate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import {RouterProvider, createBrowserRouter} from "react-router-dom";
-import StadiumLayout from "./components/Stadium/Layout";
-import MatchLayout from "./components/Match/Container";
-import RootLayout from "./pages/Root";
-import Home from "./pages/Home";
-import ErrorPage from "./pages/Error";
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {path: "/", element: <Home />},
-            {path: "/stadium", element: <StadiumLayout />},
-            {path: "/match", element: <MatchLayout />},
-        ],
-    },
-]);
-
+import {RouterProvider} from "react-router-dom";
+import router from "./router/router";
 function App() {
     return (
         <>
