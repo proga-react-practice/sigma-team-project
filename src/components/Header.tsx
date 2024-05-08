58e405ecbe76ec58acdc873aa01d2d1986eb4e44
Bohdan <mylyibohdan18@gmail.com>
2024-05-08 21:01:08 +0300

    remove contacts in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,14 @@ import {
     AppBar,
     Box,
     Toolbar,
-    Badge,
-    Avatar,
     ThemeProvider,
-    Tooltip,
     Menu,
-    MenuItem,
     Typography,
     IconButton,
     CardMedia,
 } from "@mui/material";
 import React from "react";
 import {theme} from "../utils/theme-2";
-import {Instagram, Telegram} from "@mui/icons-material";
 import ProgressBar from "./Match/ProgressBar";
 import {Link} from "react-router-dom";
 import sigmaLogoFavicon from "/sigma-team-favicon-black.png";
@@ -22,8 +17,6 @@ import {HOME, STADIUM_ROUTE, MATCH_ROUTE} from "../router/pathConstants";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const Header: React.FC = () => {
-    const [anchorElUser, setAnchorElUser] =
-        React.useState<HTMLButtonElement | null>(null);
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
         null
     );
@@ -32,33 +25,15 @@ const Header: React.FC = () => {
         {label: "Stadium", path: STADIUM_ROUTE},
         {label: "Match", path: MATCH_ROUTE},
     ];
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorElUser(event.currentTarget as HTMLButtonElement);
-    };
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
-
-    const handleInstagramClick = () => {
-        window.open("https://www.instagram.com/__bogg_", "_blank");
-        handleCloseUserMenu();
-    };
-
-    const handleTelegramClick = () => {
-        window.open("https://t.me/bohdan_mylyi", "_blank");
-        handleCloseUserMenu();
-    };
-
-    const contacts = ["Telegram", "Instagram"];
 
     return (
         <ThemeProvider theme={theme}>
@@ -164,96 +139,6 @@ const Header: React.FC = () => {
                                 ))}
                             </Box>
                         </Box>
-
-                        <Badge
-                            overlap="circular"
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "right",
-                            }}
-                            variant="dot"
-                            sx={{
-                                "& .MuiBadge-badge": {
-                                    backgroundColor:
-                                        theme.palette.success.light,
-                                    color: theme.palette.success.light,
-                                    boxShadow: (theme) =>
-                                        `0 0 0 2px ${theme.palette.background.paper}`,
-                                    "&::after": {
-                                        position: "absolute",
-                                        top: 0,
-                                        left: 0,
-                                        width: "100%",
-                                        height: "100%",
-                                        borderRadius: "50%",
-                                        animation:
-                                            "ripple 1.2s infinite ease-in-out",
-                                        border: "1px solid currentColor",
-                                        content: '""',
-                                    },
-                                },
-                                "@keyframes ripple": {
-                                    "0%": {
-                                        transform: "scale(.8)",
-                                        opacity: 1,
-                                    },
-                                    "100%": {
-                                        transform: "scale(2.4)",
-                                        opacity: 0,
-                                    },
-                                },
-                            }}
-                        >
-                            <Tooltip title="Contacts">
-                                <IconButton
-                                    onClick={handleOpenUserMenu}
-                                    sx={{p: 0}}
-                                >
-                                    <Avatar
-                                        alt="Bohdan Mylyi"
-                                        src="./src/images/photo_2024-03-31_18-21-22.jpg"
-                                    />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{mt: "45px"}}
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {contacts.map((contact) => (
-                                    <MenuItem
-                                        key={contact}
-                                        onClick={
-                                            contact === "Instagram"
-                                                ? handleInstagramClick
-                                                : handleTelegramClick
-                                        }
-                                    >
-                                        {contact === "Instagram" ? (
-                                            <Instagram />
-                                        ) : (
-                                            <Telegram />
-                                        )}
-                                        <Typography
-                                            textAlign="center"
-                                            sx={{ml: "6px"}}
-                                        >
-                                            {contact}
-                                        </Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Badge>
                     </Toolbar>
                 </AppBar>
             </Box>
