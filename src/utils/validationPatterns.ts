1555a99031a1a97e0d86a08c4fafa94e072b0950
OstapivIvan <ostapivivan05@gmail.com>
2024-05-10 07:26:37 +0300

    Add patters for validation

--- src/utils/validationPatterns.ts
+++ src/utils/validationPatterns.ts
@@ -1,3 +1,7 @@
 export const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
 export const specialCharsAndNumbers = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~\d]/;
 export const positiveIntegerPattern = /^\d+$/;
+export const excludeSpecialCharsPattern =
+    /^[^`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]*$/;
+export const excludeSpecialCharsAndNumbersPattern =
+    /^[^`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~\d]*$/;
