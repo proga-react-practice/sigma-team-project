5799df164b1f8d288df5441861457cc5dc9dca27
Bohdan <mylyibohdan18@gmail.com>
2024-05-12 23:13:00 +0300

    some style changes in CardList component

--- src/components/Match/CardList.tsx
+++ src/components/Match/CardList.tsx
@@ -55,57 +55,86 @@ const CardList: React.FC<CardListProps> = ({ blocks, removeBlock, updateBlock })
     >
       {blocks.map((block) => (
         <Grid
-          key={block.id}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            borderRadius: theme.spacing(6),
-            width: theme.spacing(70),
-            backgroundColor: "primary.main",
-            boxShadow: 5,
-            marginTop: theme.spacing(2),
-            textAlign: 'center',
-            "@media screen and (max-width: 426px)": {
-              width: theme.spacing(40),
-            },
-          }}
-        >
+        key={block.id}
+        sx={{
+          paddingLeft: theme.spacing(6),
+          paddingRight: theme.spacing(6),
+          display: "flex",
+          flexDirection: "column",
+          borderRadius: theme.spacing(6),
+          width: 'auto', 
+          height: 'auto',
+          backgroundColor: "primary.main",
+          boxShadow: 5,
+          marginTop: theme.spacing(2),
+          textAlign: 'center',
+          "@media screen and (max-width: 426px)": {
+            width: 'auto',
+          },
+        }}
+      >
           {editMode === block.id ? (
             <>
+            <Typography
+                sx={{
+                  marginTop: theme.spacing(3),
+                  fontSize: theme.spacing(4),
+                  fontFamily: "Platypi",
+                  fontWeight: 600,
+                }}
+              >
+                Editing Mode:
+              </Typography>
               <TextField
                 value={editedBlock.firstTeam || ""}
+                variant="standard"
                 onChange={(e) => handleChange("firstTeam", e.target.value)}
                 sx={{
+                  width: '70%',
                   fontFamily: "Platypi",
                   fontSize: theme.spacing(3),
                   marginTop: theme.spacing(2),
+                  marginLeft: theme.spacing(5),
+                  marginRight: 0
                 }}
               />
               <TextField
                 value={editedBlock.secondTeam || ""}
+                variant="standard"
                 onChange={(e) => handleChange("secondTeam", e.target.value)}
                 sx={{
+                  width: '70%',
                   fontFamily: "Platypi",
                   fontSize: theme.spacing(3),
                   marginTop: theme.spacing(2),
+                  marginLeft: theme.spacing(5),
+                  marginRight: 0
                 }}
               />
               <TextField
                 value={editedBlock.tickets || ""}
+                variant="standard"
                 onChange={(e) => handleChange("tickets", e.target.value)}
                 sx={{
+                  width: '70%',
                   fontFamily: "Platypi",
                   fontSize: theme.spacing(3),
                   marginTop: theme.spacing(2),
+                  marginLeft: theme.spacing(5),
+                  marginRight: 0
                 }}
               />
               <TextField
                 value={editedBlock.stadium || ""}
+                variant="standard"
                 onChange={(e) => handleChange("stadium", e.target.value)}
                 sx={{
+                  width: '70%',
                   fontFamily: "Platypi",
                   fontSize: theme.spacing(3),
                   marginTop: theme.spacing(2),
+                  marginLeft: theme.spacing(5),
+                  marginRight: 0
                 }}
               />
               <Button
