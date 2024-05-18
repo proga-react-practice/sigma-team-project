1646eebd3d85720baff19e17ddc9ffc55ee75470
OstapivIvan <ostapivivan05@gmail.com>
2024-05-18 15:08:29 +0300

    Fix color animation

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -29,7 +29,7 @@ const NotFound = () => {
                     component={Link}
                     to={HOME}
                     variant="contained"
-                    color="secondary"
+                    color="primary"
                 >
                     Back to home{" "}
                 </Button>
