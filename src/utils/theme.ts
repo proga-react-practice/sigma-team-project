711606d17a18dcde12b535f30dd31461f4ef892f
OstapivIvan <ostapivivan05@gmail.com>
2024-05-18 14:37:30 +0300

    Update theme by removing palette and using colors from theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,26 +1,10 @@
 import {createTheme} from "@mui/material/styles";
-import {teal} from "@mui/material/colors";
 
 const theme = createTheme({
-    palette: {
-        primary: teal,
-        secondary: {
-            main: "#10776b",
-        },
-        background: {
-            default: "#f0f8ff",
-        },
-        text: {
-            primary: "#0C0C0C",
-            secondary: "#10776b",
-        },
-    },
-
     components: {
         MuiInputLabel: {
             styleOverrides: {
                 root: ({theme}) => ({
-                    fontSize: "18px",
                     color: theme.palette.primary.dark,
                 }),
             },
@@ -38,24 +22,24 @@ const theme = createTheme({
                 root: ({theme}) => ({
                     width: "100%",
                     backgroundColor: theme.palette.background.default,
+                    icon: {
+                        color: theme.palette.primary.main,
+                    },
                 }),
-                icon: {
-                    color: teal[500],
-                },
             },
         },
         MuiCard: {
             styleOverrides: {
                 root: ({theme}) => ({
-                    border: "2px solid #009879",
+                    border: `2px solid${theme.palette.primary.main}`,
                     borderRadius: "5px",
                     padding: "10px",
                     marginBottom: "10px",
                     backgroundColor: theme.palette.background.default,
-                    boxShadow: "10px 7px 10px #20a78c",
+                    boxShadow: `10px 7px 10px ${theme.palette.primary.main}`,
                     transition: "box-shadow 0.3s",
                     "&:hover": {
-                        boxShadow: "0px 0px 10px #20a78c",
+                        boxShadow: `0px 0px 10px ${theme.palette.primary.main}`,
                     },
                 }),
             },
@@ -64,7 +48,6 @@ const theme = createTheme({
             styleOverrides: {
                 root: ({theme}) => ({
                     width: "100%",
-
                     "&:hover .MuiOutlinedInput-notchedOutline": {
                         borderColor: theme.palette.primary.main,
                         borderWidth: "3px",
@@ -72,32 +55,25 @@ const theme = createTheme({
                     },
                     "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
                         borderColor: theme.palette.primary.main,
-                        boxShadow: `0 0 10px ${teal[500]}`,
+
                         borderWidth: "3px",
                         transition: "border-color 0.5s, box-shadow 0.5s",
-                        transform: "translateY(-2px)",
+                    },
+                    ".MuiOutlinedInput-notchedOutline": {
+                        borderColor: theme.palette.primary.main,
+                        borderWidth: "2px",
                     },
                 }),
-                notchedOutline: {
-                    borderColor: teal[300],
-                    borderWidth: "2px",
-                },
-                input: {
-                    color: teal[900],
-                },
             },
         },
         MuiMenuItem: {
             styleOverrides: {
-                root: {
+                root: ({theme}) => ({
                     padding: "10px",
                     "&:hover": {
-                        backgroundColor: teal[200],
+                        backgroundColor: theme.palette.primary.main,
                     },
-                    "&:selected": {
-                        backgroundColor: teal[300],
-                    },
-                },
+                }),
             },
         },
     },
